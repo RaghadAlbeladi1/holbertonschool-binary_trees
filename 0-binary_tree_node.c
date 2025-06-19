3e2983ab6191eddbbf036111cf92ac6707d7bb05
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-06-19 11:01:56 +0000

    add binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,16 +10,16 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)  /* Check for memory allocation failure */
-		return (NULL);
+new_node = malloc(sizeof(binary_tree_t));
+if (!new_node)  /* Check for memory allocation failure */
+return (NULL);
 
-	new_node->n = value;  /* Assign value to the node */
-	new_node->parent = parent;  /* Assign parent pointer */
-	new_node->left = NULL;  /* Left child is NULL by default */
-	new_node->right = NULL;  /* Right child is NULL by default */
+new_node->n = value;  /* Assign value to the node */
+new_node->parent = parent;  /* Assign parent pointer */
+new_node->left = NULL;  /* Left child is NULL by default */
+new_node->right = NULL;  /* Right child is NULL by default */
 
-	return (new_node);
+return (new_node);
 }
