c9c34f55504724413cc76b2f4daa9cd463f5b83f
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-06-19 12:28:23 +0000

    add header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,10 +1,9 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-#include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -13,13 +12,12 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-
 struct binary_tree_s
 {
-int n;
-struct binary_tree_s *parent;
-struct binary_tree_s *left;
-struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -27,6 +25,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -47,4 +46,4 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-#endif /* BINARY_TREES_H */
+#endif
